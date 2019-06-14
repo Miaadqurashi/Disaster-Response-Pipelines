7fc3fd1b1d13745f7cf3417415dc2b2729c25b30
Miaadqurashi <Mhqurashi@windowslive.com>
2019-06-14 21:16:31 +0200

    type: feat  On branch master  Initial commit Created the ETL pipeline. Step by step analysys in ETL Pipeline Prearation.ipynb. Pipeline script in process_data.py  Changes to be committed: 	new file:   README.md 	new file:   app/run.py 	new file:   app/templates/go.html 	new file:   app/templates/master.html 	new file:   data/DisasterResponse.db 	new file:   data/ETL Pipeline Preparation.ipynb 	new file:   data/YourDatabaseName.db 	new file:   data/disaster_categories.csv 	new file:   data/disaster_messages.csv 	new file:   data/process_data.py 	new file:   models/train_classifier.py

---
+++ data/process_data.py
@@ -0,0 +1,59 @@
+import sys
+import re
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages,categories
+
+
+def clean_data(messages,categories):
+    s = categories.categories[0]
+    columns = re.compile("-\d;").split(s)
+    columns[-1] = columns[-1][:-2] # last label has a number at the end.
+    categories = pd.concat([categories.id,
+                 categories.categories.str.split(';{0,1}\D*-',expand=True).loc[:,1:]],
+                       axis=1)
+    categories.columns = ['id']+ columns
+    for column in categories:
+        # convert to ints
+        categories[column] = categories[column].astype(int)
+    df = pd.merge(messages,categories,on='id')
+    return df
+
+
+def save_data(df, database_filename):
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('database_filename[:-3]', engine, index=False)  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        messages,categories = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(messages,categories)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
